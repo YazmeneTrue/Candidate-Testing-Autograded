ec4a5681c36b5d6f66f46f27aa3cbb20abf2066e
Yazzy Boo <yazzyboo@Chazzs-MacBook-Pro.local>
2024-01-11 21:41:32 -0500

    having issues printing question

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,11 +3,11 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = '';
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = "Who was the first American woman in Space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = '';
 
 
 //TODO: Variables for Part 2
@@ -18,11 +18,15 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
+  const input = require('readline-sync');
+
+let candiateName = input.question(" Name: ")
 
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
+  const input = require('readline-sync');
 
 
 }
@@ -42,7 +46,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log();
+   console.log("Hello " + candidateName);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
